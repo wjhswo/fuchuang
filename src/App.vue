ac3683f7580a42c48befe995b14ba5f634402092
hy <yeahhuangyue@163.com>
2024-03-14 22:05:42 +0800

    [fix] 修改项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,5 @@
 <template>
     <div id="app">
-        <LogIn v-if="false"/>
-        <MainInterfaceForUser v-if="true"/>
-  </div>
-  <div id="hahaha">
-        <router-link to="/">hahahah</router-link>
         <router-view></router-view>
-  </div>
+    </div>
 </template>
-
-<script>
-import LogIn from "./views/LogIn.vue"
-import MainInterfaceForUser from "./views/MainInterfaceForUser.vue"
-
-export default{
-  components:{
-    LogIn,
-    MainInterfaceForUser,
-  }
-}
-</script>
-
-<style>
-#hahaha{
-  background-color: #68ed3c;
-  display: flex;
-  justify-content: center;
-}
-
-html{
-  height: 100%;
-}
-body{
-  height:100%;
-  margin:0px;
-  background-color: rgb(241, 241, 241);
-}
-#app{
-  height:100%;
-}
-.hide{
-  display: hide;
-}
-</style>./views/MainInterfaceForUser.vue

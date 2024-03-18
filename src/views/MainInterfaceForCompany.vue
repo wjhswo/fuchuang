8c8e577a0f70e8d0b6b478438973bda277bf7b03
hy <yeahhuangyue@163.com>
2024-03-18 22:41:15 +0800

    [feat] - 添加arco-design组件库

--- src/views/MainInterfaceForCompany.vue
+++ src/views/MainInterfaceForCompany.vue
@@ -5,6 +5,9 @@
         <GetResume v-if="menu[1]"/>
         <CompanyInformation v-if="menu[0]"/>
     </div>
+    <div id="hahahah">
+        {{ ans }}
+    </div>
 </template>
 
 <script>
@@ -12,6 +15,7 @@ import CompanyMenuBar from "../components/CompanyMenuBar.vue"
 import GetResume from "../components/GetResume.vue"
 import CompanyInformation from "../components/CompanyInformation.vue"
 import HeadPart from "../components/HeadPart.vue"
+import axios from "axios"
 
 export default{
     components:{
@@ -23,19 +27,44 @@ export default{
     data(){
         return{
             menu:[true,false],
+            ans: {}
         }
     },
     methods:{
         getMenu(menu){
             this.menu=menu;
-        }
+        },
+    },
+
+    mounted() {
+        axios
+                .get('https://yesno.wtf/api')
+                .then( (res) => {
+                    console.log(res)
+                    this.ans = res.data.answer
+                })
     }
 }
+
+// mounted () {
+//     axios
+//             .get('https://www.runoob.com/try/ajax/json_demo.json')
+//             .then(function (res) {
+//         console.log(res)
+//     })
+// }
+
+
+
 </script>
 
 <style scoped>
 #main{
     margin-left: 250px;
     margin-top: 140px;
+}
+#hahahah {
+    background-color: blueviolet;
+    margin: 500px;
 }
 </style>
